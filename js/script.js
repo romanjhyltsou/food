975433f1bba56868a7f2c490ff14a269e238d81d
romanjhyltsou <romanjhyltsou@gmail.com>
2023-04-28 13:11:06 +0300

    feat: added feach form

--- js/script.js
+++ js/script.js
@@ -132,8 +132,7 @@ window.addEventListener('DOMContentLoaded', ()=> {
     // Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalClosebtn = document.querySelector('[data-close]');
+          modal = document.querySelector('.modal');
 
     function openModal(){
       modal.classList.add('show');
@@ -150,10 +149,9 @@ window.addEventListener('DOMContentLoaded', ()=> {
       document.body.style.overflow = '';
     }
 
-    modalClosebtn.addEventListener('click', closeModal);
 
     modal.addEventListener('click', (e)=> {
-      if(e.target === modal){
+      if(e.target === modal || e.target.getAttribute('data-close') == ''){
         closeModal();
       }
     });
@@ -226,7 +224,7 @@ window.addEventListener('DOMContentLoaded', ()=> {
    const forms = document.querySelectorAll('form');
 
    const message = {
-      loading: 'Загрузка',
+      loading: 'img/form/spinner.svg',
       success: 'Спасибл! Скоро мы с вами свяжемся',
       failure: 'Что-то пошло не так...'
    };
@@ -239,42 +237,65 @@ window.addEventListener('DOMContentLoaded', ()=> {
       form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const statusMessage = document.createElement('div');
-        statusMessage.classList.add('status');
-        statusMessage.textContent = message.loading;
-        form.append(statusMessage);
-
-        const request = new XMLHttpRequest();
-        request.open('POST', 'js/server.php');
-
-       // request.setRequestHeader('Content-type', 'application/json'); /* для json */
+        const statusMessage = document.createElement('img');
+        statusMessage.src = message.loading;
+        statusMessage.style.cssText = `
+          display: block;
+          margin: 0 auto;
+        `;
+       form.insertAdjacentElement('afterend', statusMessage);
 
         const formData = new FormData(form);
-        /* для json start*/
-/*         const object = {};
+
+        const object = {};
         formData.forEach(function(value, key){
           object[key] = value;
         });
 
-        const json = JSON.stringify(object); */
-
-        /* для json end*/
-
-        request.send(formData); //json
-
-        request.addEventListener('load', ()=> {
-          if(request.status === 200){
-            console.log(request.response);
-            statusMessage.textContent = message.success;
-            form.reset();
-            setTimeout(()=> {
-              statusMessage.remove();
-            },2000);
-          }else{
-            statusMessage.textContent = message.failure;
-          }
+        fetch('js/server.php', {
+          method: "POST",
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(object)
+        })
+        .then(data => data.text())
+        .then(data => {
+          console.log(data);
+          showThanksModal(message.success);
+          form.reset();
+          statusMessage.remove();
+        }).catch(()=> {
+          showThanksModal(message.failure);
+        }).finally(()=> {
+          form.reset();
         });
+
       });
+   }
+
+   function showThanksModal(message){
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+    <div class="modal__content">
+          <div data-close class="modal__close">&times;</div>
+          <div class="modal__title">${message}</div>
+    </div>
+    `;
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(()=>{
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    },4000);
    }
 });
 
--- js/server.php
+++ js/server.php
@@ -1,5 +1,5 @@
 <?php
-//$_POST = json_decode( file_get_contents("php://input"), true );  /*  для json */
+$_POST = json_decode( file_get_contents("php://input"), true );  /*  для json */
 echo var_dump($_POST);
 /* берет данные которые пришли с клиента 
 превращает их в строку и показывает 
